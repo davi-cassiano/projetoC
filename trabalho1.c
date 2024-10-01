4e9a37ead18cbd148eb8e887e8db9f0704d709fd
luquinha27 <66984821+luquinha27@users.noreply.github.com>
2024-10-01 17:48:26 -0300

    Depositar na conta

--- trabalho1.c
+++ trabalho1.c
@@ -38,3 +38,18 @@ int main() {
 
     return 0;
 }
+
+
+// Função para depositar na conta
+void depositar(Usuario *usuario) {
+    double quantia = input2("Digite a quantidade para depositar: ");
+    if (quantia > 0) {
+        usuario->saldo += quantia;
+        char transacao[100];
+        snprintf(transacao, 100, "+ %.2f REAL\n", quantia);
+        strcpy(usuario->extrato[usuario->transacao_atual++], transacao);
+        printf("Depósito realizado com sucesso!\nSeu novo saldo é: %.2f\n", usuario->saldo);
+    } else {
+        printf("Quantidade inválida!\n");
+    }
+}
