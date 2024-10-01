3e3349d5ee2c73098e68c8a4540a20a4e5d4d3da
André Marques <160554647+de-marques@users.noreply.github.com>
2024-10-01 17:45:03 -0300

    Extrato

--- trabalho1.c
+++ trabalho1.c
@@ -38,3 +38,31 @@ int main() {
 
     return 0;
 }
+
+// Função para salvar o extrato no arquivo.txt
+void arquivo(Usuario *usuario) {
+    FILE *file = fopen("extrato.txt", "a");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo de extrato.\n");
+        return;
+    }
+    fprintf(file, "Extrato de %s (%s):\n", usuario->nome, usuario->cpf);
+    for (int i = 0; i < usuario->transacao_atual; i++) {
+        fprintf(file, "%s", usuario->extrato[i]);
+    }
+    fclose(file);
+}
+
+// Função para consultar o extrato da conta
+void consultar_extrato(Usuario *usuario) {
+    printf("\nNome: %s\n", usuario->nome);
+    printf("CPF: %s\n", usuario->cpf);
+    if (usuario->transacao_atual > 0) {
+        for (int i = 0; i < usuario->transacao_atual; i++) {
+            printf("%s", usuario->extrato[i]);
+        }
+    } else {
+        printf("Não houve transações.\n");
+    }
+    arquivo(usuario);
+}
