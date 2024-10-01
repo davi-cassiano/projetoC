cbc2d579b9e28cff7d5896cb7f579dbc9f01cb4d
davi-cassiano <160554801+davi-cassiano@users.noreply.github.com>
2024-10-01 18:31:20 -0300

    Update trabalho1.c

--- trabalho1.c
+++ trabalho1.c
@@ -4,41 +4,6 @@
 #include <string.h>
 #include <time.h>
 
-int main() {
-    srand(time(NULL)); // Inicializa o gerador de números aleatórios
-    carregar_usuarios();
-
-    // Função para método de entrada no sistema (CADASTRO, LOGIN, OU SAIR)
-    int opcao;
-    while (1) {
-        printf("\n| 1 | Cadastrar usuário\n");
-        printf("| 2 | Login\n");
-        printf("| 3 | Sair\n");
-        opcao = input1("Escolha uma opção: ");
-
-        switch (opcao) {
-            case 1:
-                cadastrar_usuario();
-                break;
-            case 2: {
-                Usuario *usuario = login_usuario();
-                if (usuario != NULL) {
-                    menu(usuario);
-                }
-                break;
-            }
-            case 3:
-                printf("Saindo do sistema...\n");
-                return 0;
-            default:
-                printf("Opção inválida.\n");
-                break;
-        }
-    }
-
-    return 0;
-}
-
 // Cotação bitcoins (globais)
 double cot_bitcoin = 322.865;
 double cot_eth = 15.524;
@@ -495,4 +460,39 @@ void menu(Usuario *usuario) {
                 break;
         }
     }
+}
+
+int main() {
+    srand(time(NULL)); // Inicializa o gerador de números aleatórios
+    carregar_usuarios();
+
+    // Função para método de entrada no sistema (CADASTRO, LOGIN, OU SAIR)
+    int opcao;
+    while (1) {
+        printf("\n| 1 | Cadastrar usuário\n");
+        printf("| 2 | Login\n");
+        printf("| 3 | Sair\n");
+        opcao = input1("Escolha uma opção: ");
+
+        switch (opcao) {
+            case 1:
+                cadastrar_usuario();
+                break;
+            case 2: {
+                Usuario *usuario = login_usuario();
+                if (usuario != NULL) {
+                    menu(usuario);
+                }
+                break;
+            }
+            case 3:
+                printf("Saindo do sistema...\n");
+                return 0;
+            default:
+                printf("Opção inválida.\n");
+                break;
+        }
+    }
+
+    return 0;
 }
