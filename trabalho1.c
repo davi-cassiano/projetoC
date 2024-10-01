8c4443bf453ded5f633ebef2e20b04bd6bcf5448
davi-cassiano <dcassiano3005@gmail.com>
2024-10-01 17:23:25 -0300

    Criação da função principal

---
+++ trabalho1.c
@@ -0,0 +1,40 @@
+// Incluir no sistema
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL)); // Inicializa o gerador de números aleatórios
+    carregar_usuarios();
+
+    // Função para método de entrada no sistema (CADASTRO, LOGIN, OU SAIR)
+    int opcao;
+    while (1) {
+        printf("\n| 1 | Cadastrar usuário\n");
+        printf("| 2 | Login\n");
+        printf("| 3 | Sair\n");
+        opcao = input1("Escolha uma opção: ");
+
+        switch (opcao) {
+            case 1:
+                cadastrar_usuario();
+                break;
+            case 2: {
+                Usuario *usuario = login_usuario();
+                if (usuario != NULL) {
+                    menu(usuario);
+                }
+                break;
+            }
+            case 3:
+                printf("Saindo do sistema...\n");
+                return 0;
+            default:
+                printf("Opção inválida.\n");
+                break;
+        }
+    }
+
+    return 0;
+}
